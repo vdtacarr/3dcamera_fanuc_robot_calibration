dd78cc7292a37fa04f127d8c198f7f168a2f9556
vdtacarr <49876158+vdtacarr@users.noreply.github.com>
2021-08-13 09:46:11 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnCalibrate = new System.Windows.Forms.Button();
+            this.lblReceived = new System.Windows.Forms.Label();
+            this.lblSended = new System.Windows.Forms.Label();
+            this.txtClientIp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.txtConsole2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(253, 17);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(94, 29);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            // 
+            // btnCalibrate
+            // 
+            this.btnCalibrate.Location = new System.Drawing.Point(247, 297);
+            this.btnCalibrate.Name = "btnCalibrate";
+            this.btnCalibrate.Size = new System.Drawing.Size(94, 29);
+            this.btnCalibrate.TabIndex = 1;
+            this.btnCalibrate.Text = "Calibrate";
+            this.btnCalibrate.UseVisualStyleBackColor = true;
+            // 
+            // lblReceived
+            // 
+            this.lblReceived.AutoSize = true;
+            this.lblReceived.Location = new System.Drawing.Point(122, 105);
+            this.lblReceived.Name = "lblReceived";
+            this.lblReceived.Size = new System.Drawing.Size(219, 20);
+            this.lblReceived.TabIndex = 2;
+            this.lblReceived.Text = "-----------------------------------";
+            // 
+            // lblSended
+            // 
+            this.lblSended.AutoSize = true;
+            this.lblSended.Location = new System.Drawing.Point(122, 146);
+            this.lblSended.Name = "lblSended";
+            this.lblSended.Size = new System.Drawing.Size(225, 20);
+            this.lblSended.TabIndex = 4;
+            this.lblSended.Text = "------------------------------------";
+            // 
+            // txtClientIp
+            // 
+            this.txtClientIp.Location = new System.Drawing.Point(95, 19);
+            this.txtClientIp.Name = "txtClientIp";
+            this.txtClientIp.Size = new System.Drawing.Size(133, 27);
+            this.txtClientIp.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "robot IP:";
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(438, 272);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.Size = new System.Drawing.Size(194, 91);
+            this.txtConsole.TabIndex = 7;
+            // 
+            // txtConsole2
+            // 
+            this.txtConsole2.Location = new System.Drawing.Point(438, 207);
+            this.txtConsole2.Name = "txtConsole2";
+            this.txtConsole2.Size = new System.Drawing.Size(194, 27);
+            this.txtConsole2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtConsole2);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtClientIp);
+            this.Controls.Add(this.lblSended);
+            this.Controls.Add(this.lblReceived);
+            this.Controls.Add(this.btnCalibrate);
+            this.Controls.Add(this.btnStartServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCalibrate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSended;
+        private System.Windows.Forms.TextBox txtClientIp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblReceived;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.TextBox txtConsole2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using iRoVisionCore;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using iRoVisionCore.Communication;
+using HalconDotNet;
+using Zivid.NET.Calibration;
+using Zivid.NET;
+using System.IO;
+using System.Data;
+using System.Linq;
+
+namespace server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Logger.Initialize();
+        }
+
+        SocketController sc = new SocketController();
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            btnStartServer.Tag = false;
+            HPose pose = new HPose(0.1, 0.1, 0.1, 0.0, 0.0, 0.0, "Rp+T", "gba", "point");
+            HTuple tuple = new HTuple();
+            HTuple tuple2 = new HTuple(0, 0, 0, 1);
+
+            int index = 0;
+
+            sc.ConnectedClient += (client) =>
+            {
+                if (client == null)
+                    return;
+
+                if (!client.Info.Initialized)
+                {
+                    client.Connected += (info, state) =>
+                    {
+                        this.Invoke(new Action(() => SetColor(state)));
+                    };
+
+                    client.Received += (info, message) =>
+                    {
+                        this.Invoke(new Action(() => SetText(lblReceived, message.Message)));
+                        for (int i = 0; i < message.Pose.Count; i++)
+                        {
+                            pose[i] = message.Pose[i];
+                        }
+
+                        HOperatorSet.PoseToHomMat3d(pose, out tuple);
+                        tuple.Append(tuple2);
+                        index++;
+                        File.WriteAllText("C:/Users/vedatacar/OneDrive - Robsen Robotics/Desktop/calibration/14/calibration_robot/" + index.ToString() + ".yaml", string.Join(" ", tuple) + Environment.NewLine);
+
+                        var respons = new SocketArgs("0", "0", ProcessStatus.OK, new List<string>(), new List<double>() { 0.0, 0.0, 0.0, 0.0, 0.0 }, ProcessType.MATCHING);
+
+
+                        client.SendResult(respons);
+
+                        // For sended Message
+                        this.Invoke(new Action(() => SetText(lblSended, respons.Message)));
+                    };
+
+                    client.Info.Initialized = true;
+
+                }
+            };
+
+        }
+
+        private void SetColor(bool state)
+        {
+            txtClientIp.BackColor = state ? Color.Green : Color.White;
+        }
+
+        private void SetText(Label box, string text)
+        {
+            box.Text = text;
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (txtClientIp.Text == string.Empty)
+                txtClientIp.Text = sc.GetClientByID(0).Info.IPAddress;
+
+            if (!(bool)btn.Tag)
+            {
+                if (sc.GetClients().Count == 1)
+                    sc.RegisterClient(new ClientInfo() { IPAddress = txtClientIp.Text, Name = "Robot" });
+
+                sc.StartServer(null);
+                btn.Tag = true;
+                btn.Text = "Stop Server";
+            }
+            else
+            {
+                sc.StopServer();
+                btn.Tag = false;
+                btn.Text = "Start Server";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            sc.SaveConfig();
+            sc.StopServer();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnCalibrate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                HTuple mat3D = new HTuple();
+                HTuple pose = new HTuple();
+
+                var parameters = new CameraIntrinsics.DistortionGroup();
+                double[] matrixGroup = { parameters.K1, parameters.K3, parameters.K3, parameters.P1, parameters.P2, };
+                
+                //Console.WriteLine("Connecting to camera");
+                //var camera = zivid.ConnectCamera();
+                var zivid = new Zivid.NET.Application();
+                var inputs = ReadInputs();
+
+                var calibrationResult = Calibrator.CalibrateEyeToHand(inputs);
+                txtConsole.Text = calibrationResult.ToString();
+
+                if (calibrationResult)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            if ((4 * i + j) == 3 || (4 * i + j) == 7 || (4 * i + j) == 11)
+                            {
+                                calibrationResult.Transform()[i, j] = (calibrationResult.Transform()[i, j] / 1000);
+                            }
+                            mat3D[(4 * i + j)] = calibrationResult.Transform()[i, j];
+
+
+                        }
+                    }
+
+                    //File.AppendAllText("C:/Users/vedatacar/OneDrive - Robsen Robotics/Desktop/calibration/6/result.yaml", Hommat3D2.ToString() + Environment.NewLine);
+
+                    HOperatorSet.HomMat3dToPose(mat3D, out pose);
+                    File.WriteAllText("C:/Users/vedatacar/OneDrive - Robsen Robotics/Desktop/calibration/14/result.yaml", pose.ToString() + Environment.NewLine);
+                    File.AppendAllText("C:/Users/vedatacar/OneDrive - Robsen Robotics/Desktop/calibration/14/result.yaml", calibrationResult.ToString());
+
+                }
+                else
+                {
+                    Environment.ExitCode = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Environment.ExitCode = 1;
+                txtConsole.Text = ex.Message;
+            }
+        }
+
+        public List<HandEyeInput> ReadInputs()
+        {
+            var input = new List<HandEyeInput>();
+            Interaction.ExtendInputBuffer(2048);
+            int[] files = { 5, 4, 3, 2, 1 };
+
+            foreach (int index in files)
+            {
+
+
+                try
+                {
+
+                    var robotPose = Interaction.ReadRobotPose(index);
+
+
+                    using (var frame = Interaction.ReadZDFFromFile(index))
+                    {
+
+
+                        var result = Detector.DetectFeaturePoints(frame.PointCloud);
+
+                        if (result)
+                        {
+
+                            input.Add(new HandEyeInput(robotPose, result));
+                            this.Invoke(new Action(() =>
+                            {
+                                txtConsole2.Text = index.ToString() + ".pose  " + "Başarılı bir şekilde tespit edildi. ";
+                                txtConsole2.Refresh();
+                            }));
+
+                        }
+                        else
+                        {
+                            txtConsole2.Text = index.ToString() + "bulunamadı.";
+                            txtConsole2.Refresh();
+
+                        }
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    txtConsole.Text = ex.Message;
+                    continue;
+                }
+            }
+
+            return input;
+        }
+
+       
+    }
+
+    class Interaction
+    {
+
+        // Console.ReadLine only supports reading 256 characters, by default. This limit is modified
+        // by calling ExtendInputBuffer with the maximum length of characters to be read.
+        public static void ExtendInputBuffer(int size)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(), Console.InputEncoding, false, size));
+        }
+
+
+        public static Zivid.NET.Calibration.Pose ReadRobotPose(int index)
+        {
+
+            var input = File.ReadAllText("C:/Users/vedatacar" +
+                "/OneDrive - Robsen Robotics/Desktop/calibration/14/calibration_robot/" + index.ToString() + ".yaml");
+
+
+            var elements = input.Split().Where(x => !string.IsNullOrEmpty(x.Trim())).Select(x => float.Parse(x)).ToArray();
+
+            var robotPose = new Zivid.NET.Calibration.Pose(elements);
+            return robotPose;
+        }
+
+
+        public static Zivid.NET.Frame ReadZDFFromFile(int index)
+        {
+            var dataFile =
+     ("C:/Users/vedatacar" + "/OneDrive - Robsen Robotics/Desktop/calibration/14/calibration_zivid/" + index.ToString() + ".zdf");
+
+
+            var frame = new Zivid.NET.Frame(dataFile);
+
+
+            return frame;
+        }
+    }
+}
